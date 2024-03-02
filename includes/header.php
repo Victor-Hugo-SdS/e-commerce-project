59b62c65c15bc852eebb68a5ccd9186917061e92
Victor Hugo <vhsds2000@gmail.com>
2024-03-02 15:12:24 -0400

    project creation and structuring + header

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- CSS -->
+    <link rel="stylesheet" href="src/style.css">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <title>E-Market</title>
+</head>
+<body>
+    
+    <header>
+        <div class="container">
+            <div class="row justify-content-center justify-content-md-between align-items-center gap-3 gap-md-0 py-4">
+                <a href="/index.php" class="col-6"><img src="img/E-MARKET.png" alt="Logo E-Market"></a>
+                <div class="col-12 col-md-4 col-xl-3">
+                    <form class="search-area rounder-3 p-3 d-flex flex-nowrap">
+                        <i class="bi bi-search me-2"></i>
+                        <input type="text" placeholder="Search Products">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </header>
