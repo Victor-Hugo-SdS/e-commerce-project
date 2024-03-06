acf1cf6126857430312b9ef85f394be42b3c1dd1
Victor Hugo <vhsds2000@gmail.com>
2024-03-06 16:17:06 -0400

    finishing carousel home

---
+++ home.php
@@ -0,0 +1,47 @@
+<?php require_once('includes/header.php');?>
+
+    <section>
+        <div id="carouselExampleCaptions" class="carousel slide carousel-home">
+
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="carousel-caption d-none d-md-block">
+                        <div class="container">
+                            <h2>Buy more easily!</h2>
+                            <p class="py-4">Discover a world of possibilities in our store, where every click<br> is a new shopping adventure!</p>
+                            <a href="#" class="btn-carousel">Find out more</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <div class="carousel-caption d-none d-md-block">
+                        <div class="container">
+                            <h2>Buy more easily!</h2>
+                            <p class="py-4">Discover a world of possibilities in our store, where every click<br> is a new shopping adventure!</p>
+                            <a href="#" class="btn-carousel">Find out more</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <div class="carousel-caption d-none d-md-block">
+                        <div class="container">
+                            <h2>Buy more easily!</h2>
+                            <p class="py-4"Discover a world of possibilities in our store, where every click<br> is a new shopping adventure!</p>
+                            <a href="#" class="btn-carousel">Find out more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+<?php require_once('includes/footer.php');?>
